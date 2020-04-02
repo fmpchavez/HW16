389f62aa8b8d24c2c0d6ac2157c8aad31b3fc1cb
Ferry Chavez <ferry.chavez@gmail.com>
2020-04-01 22:12:13 -0700

    ready for deployment

---
+++ scripts/seedDB.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(
+    process.env.MONGODB_URI ||
+    "mongodb://localhost/googleboks"
+);
+
+const bookSeed =
+{
+    authors: ["Michelle Obama"],
+    description: "An intimate, powerful, and inspiring memoir by the former First Lady of the United States #1 NEW YORK TIMES BESTSELLER • OPRAH’S BOOK CLUB PICK • NAACP IMAGE AWARD WINNER In a life filled with meaning and accomplishment, Michelle Obama has emerged as one of the most iconic and compelling women of our era. As First Lady of the United States of America—the first African American to serve in that role—she helped create the most welcoming and inclusive White House in history, while also establishing herself as a powerful advocate for women and girls in the U.S. and around the world, dramatically changing the ways that families pursue healthier and more active lives, and standing with her husband as he led America through some of its most harrowing moments. Along the way, she showed us a few dance moves, crushed Carpool Karaoke, and raised two down-to-earth daughters under an unforgiving media glare. In her memoir, a work of deep reflection and mesmerizing storytelling, Michelle Obama invites readers into her world, chronicling the experiences that have shaped her—from her childhood on the South Side of Chicago to her years as an executive balancing the demands of motherhood and work, to her time spent at the world’s most famous address. With unerring honesty and lively wit, she describes her triumphs and her disappointments, both public and private, telling her full story as she has lived it—in her own words and on her own terms. Warm, wise, and revelatory, Becoming is the deeply personal reckoning of a woman of soul and substance who has steadily defied expectations—and whose story inspires us to do the same.",
+    image: "http://books.google.com/books/content?id=YbtNDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+    link: "https://play.google.com/store/books/details?id=YbtNDwAAQBAJ",
+    title: "Becoming",
+}
+
+
+db.Book
+    .remove({})
+    .then(() => db.Book.collection.insertMany(bookSeed))
+    .then(data => {
+        console.log(data.result.n + " records inserted!");
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
